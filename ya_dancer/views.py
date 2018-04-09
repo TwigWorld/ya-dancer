2b7b526c1cce7be2afc517ac2d1210034c851ca7
Ioannis Cleary <icleary@twig-world.com>
2018-04-09 15:00:22 +0100

    Update views.py

--- ya_dancer/views.py
+++ ya_dancer/views.py
@@ -10,6 +10,7 @@ from .models import HealthTable, MongoHealthDocument
 class HealthCheckView(object):
 
     def _check_db(self):
+        self._delete_session()
         try:
             obj = HealthTable.objects.create(health_field="test")
             obj.health_field = "newtest"
@@ -20,6 +21,7 @@ class HealthCheckView(object):
             return str(e)
 
     def _check_mongo(self):
+        self._delete_session()
         try:
             import mongoengine as mongo
             all_connection_settings = mongo.connection._connection_settings
@@ -42,6 +44,7 @@ class HealthCheckView(object):
             return str(e)
 
     def _default_dict(self):
+        self._delete_session()
         response_dict = {
             'app_status': 'ok'
         }
@@ -62,6 +65,7 @@ class HealthCheckView(object):
 class HealthCheckDatabaseView(HealthCheckView, View):
 
     def get(self, request, *args, **kwargs):
+        self._delete_session()
         response_dict = {'db': self._check_db()}
         return self._json_response(response_dict)
 
@@ -69,6 +73,7 @@ class HealthCheckDatabaseView(HealthCheckView, View):
 class HealthCheckAppView(HealthCheckView, View):
 
     def get(self, request, *args, **kwargs):
+        self._delete_session()
         response_dict = self._default_dict()
         return self._json_response(response_dict)
 
@@ -84,6 +89,7 @@ class HealthCheckRedisView(HealthCheckView, View):
 class HealthCheckMongoView(HealthCheckView, View):
 
     def get(self, request, *args, **kwargs):
+        self._delete_session()
         response_dict = {'mongo_db': self._check_mongo()}
         return self._json_response(response_dict)
 
@@ -91,6 +97,7 @@ class HealthCheckMongoView(HealthCheckView, View):
 class HealthCheckAllView(HealthCheckView, View):
 
     def get(self, request, *args, **kwargs):
+        self._delete_session()
         response_dict = self._default_dict()
         response_dict['db'] = self._check_db()
         if self.request.GET.get('mongodb', ''):
