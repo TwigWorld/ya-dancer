b0f912658c536540217000ab6ce62bd71d32c926
Colin Ross <cross@twig-world.com>
2021-06-23 16:43:51 +0100

    Django 1.11 fixes

--- ya_dancer/urls.py
+++ ya_dancer/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import (
     HealthCheckDatabaseView,
@@ -6,14 +6,14 @@ from .views import (
     HealthCheckElasticSearchView,
     HealthCheckMongoView,
     HealthCheckAllView,
-    HealthCheckAppView
+    HealthCheckAppView,
 )
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', HealthCheckAllView.as_view(), name='health_check_status'),
     url(r'^db/$', HealthCheckDatabaseView.as_view(), name='health_check_db_status'),
     url(r'^redis/$', HealthCheckRedisView.as_view(), name='health_check__redis_status'),
     url(r'^elastic-search/$', HealthCheckElasticSearchView.as_view(), name='health_check_elasticsearch_status'),
     url(r'^mongodb/$', HealthCheckMongoView.as_view(), name='health_check_mongodb_status'),
-    url(r'^app/$', HealthCheckAppView.as_view(), name='health_check_elasticsearch_status'),        
-)
+    url(r'^app/$', HealthCheckAppView.as_view(), name='health_check_elasticsearch_status'),
+]
