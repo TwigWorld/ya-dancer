269144a238a395ea026b319d3e6a5be230ad417d
Colin Ross <cross@twig-world.com>
2022-09-26 15:29:53 +0100

    .

--- runtests.py
+++ runtests.py
@@ -4,34 +4,15 @@ import django
 from django.core.management import call_command
 
 
-# path to envoy
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 django.conf.settings.configure(
     DEBUG=True,
     USE_TZ=True,
-    DATABASES={
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-        }
-    },
     INSTALLED_APPS=[
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "django.contrib.sites",
         "ya_dancer",
     ],
     ROOT_URLCONF='ya_dancer.urls',
-    SITE_ID=1,
-    TEMPLATES=[
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'APP_DIRS': True,
-            'OPTIONS': {
-                "debug": True,  # We want template errors to raise
-            },
-        },
-    ],
 )
 
 django.setup()
