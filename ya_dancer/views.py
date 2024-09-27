c9bff77a1e26972a9943a9b895ce6ccc83831fa9
sky-1998 <shivam.yadav@imaginelearning.com>
2024-09-27 13:54:15 +0530

    reformatted code with black

--- ya_dancer/views.py
+++ ya_dancer/views.py
@@ -1,9 +1,10 @@
 import json
 
-from django.views.generic.base import View
 from django.http import HttpResponse
+from django.views.generic.base import View
 
-from .models import HealthTable, MongoHealthDocument
+from .models import HealthTable
+from .models import MongoHealthDocument
 
 
 class HealthCheckView(object):
@@ -14,50 +15,46 @@ class HealthCheckView(object):
             obj.health_field = "newtest"
             obj.save()
             obj.delete()
-            return 'ok'
+            return "ok"
         except Exception as e:
             return str(e)
 
     def _check_mongo(self):
         try:
             import mongoengine as mongo
+
             all_connection_settings = mongo.connection._connection_settings
 
             for settings_key in mongo.connection._connection_settings.keys():
                 connection_setting = all_connection_settings[settings_key]
                 mongo.connect(
-                    connection_setting['name'],
+                    connection_setting["name"],
                     alias=settings_key,
-                    host=connection_setting['host'],
-                    port=connection_setting['port'],
-                    username=connection_setting['username'],
-                    password=connection_setting['password']
+                    host=connection_setting["host"],
+                    port=connection_setting["port"],
+                    username=connection_setting["username"],
+                    password=connection_setting["password"],
                 )
                 obj = MongoHealthDocument(health_field="newtest")
                 obj.save()
                 obj.delete()
-                return 'ok'
+                return "ok"
         except Exception as e:
             return str(e)
 
     def _default_dict(self):
-        response_dict = {
-            'app_status': 'ok'
-        }
+        response_dict = {"app_status": "ok"}
         return response_dict
 
     def _json_response(self, response_dict):
         response_json = json.dumps(response_dict)
-        return HttpResponse(
-            response_json,
-            content_type='application/json'
-        )
+        return HttpResponse(response_json, content_type="application/json")
 
 
 class HealthCheckDatabaseView(HealthCheckView, View):
 
     def get(self, request, *args, **kwargs):
-        response_dict = {'db': self._check_db()}
+        response_dict = {"db": self._check_db()}
         return self._json_response(response_dict)
 
 
@@ -79,7 +76,7 @@ class HealthCheckRedisView(HealthCheckView, View):
 class HealthCheckMongoView(HealthCheckView, View):
 
     def get(self, request, *args, **kwargs):
-        response_dict = {'mongo_db': self._check_mongo()}
+        response_dict = {"mongo_db": self._check_mongo()}
         return self._json_response(response_dict)
 
 
@@ -87,7 +84,7 @@ class HealthCheckAllView(HealthCheckView, View):
 
     def get(self, request, *args, **kwargs):
         response_dict = self._default_dict()
-        response_dict['db'] = self._check_db()
-        if self.request.GET.get('mongodb', ''):
-            response_dict['mongo_db'] = self._check_mongo()
+        response_dict["db"] = self._check_db()
+        if self.request.GET.get("mongodb", ""):
+            response_dict["mongo_db"] = self._check_mongo()
         return self._json_response(response_dict)
